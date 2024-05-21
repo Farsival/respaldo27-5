150548202f0bd7920aa429fa2504b9fd9827e09d
Fedequeso <fedequeso@gmail.com>
2024-05-20 21:12:17 -0300

    V3

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  // Otras rutas...
+  {
+    path: '/',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('@/views/Home.vue'),
+      },
+    ],
+  },
+  {
+    path: '/cursos',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Cursos',
+        component: () => import('@/views/Cursos.vue'),
+      },
+    ]
+  },
+  {
+    path: '/noticias',
+    component: () => import('@/layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Noticias',
+        component: () => import('@/views/Noticias.vue'),
+      },
+    ]
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
---
+++ src/views/Cursos.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container class="text-center">
+    <v-row justify="center">
+      <v-col cols="12" md="4" sm="6" v-for="(action, index) in actions" :key="index">
+        <v-card
+          class="mx-auto"
+          max-width="500"
+          :style="{ backgroundColor: action.currentColor }"
+          @mouseover="toggleColors(index)"
+          @click="handleActionClick(action)"
+        >
+          <v-container fluid>
+            <v-row dense>
+              <v-col :cols="12">
+                  <v-img
+                    :src="action.src"
+                    class="align-end"
+                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                    height="200px"
+                    cover
+                  >
+                    <v-card-title class="text-white">{{ action.label }}</v-card-title>
+                  </v-img>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="medium-emphasis"
+                      icon="mdi mdi-pen-plus"
+                      size="small"
+                    ></v-btn>
+                    <v-btn
+                      color="medium-emphasis"
+                      icon="mdi mdi-book-edit"
+                      size="small"
+                    ></v-btn>
+                    <v-btn
+                      color="medium-emphasis"
+                      icon="mdi mdi-delete-alert"
+                      size="small"
+                    ></v-btn>
+                  </v-card-actions>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      actions: [
+        // Tus otras acciones...
+        { 
+          label: 'Cursos', 
+          route: '/add_course', 
+          color: 'blue darken-2', 
+          rounded: '0', 
+          currentColor: 'blue darken-2', 
+          src: 'https://www.mundodeportivo.com/urbantecno/hero/2023/03/descubre-cuales-son-las-mejores-webs-para-hacer-cursos-online-gratis-y-de-pago.jpg?width=1200&aspect_ratio=16:9' // Imagen de ejemplo, cambia según sea necesario
+        },
+        // Tus otras acciones...
+      ]
+    };
+  },
+  methods: {
+    handleActionClick(action) {
+      if (action.route) {
+        this.$router.push(action.route);
+      } else {
+        // Aquí puedes manejar otras acciones si es necesario
+      }
+    },
+    toggleColors(index) {
+      const tempColor = this.actions[index].color;
+      this.actions[index].color = this.actions[index].currentColor;
+      this.actions[index].currentColor = tempColor;
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
